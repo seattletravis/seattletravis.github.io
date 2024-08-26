efd32af8ac4927a7ac14f4b977a2656a7594cf16
Travis Lamberte <travis@lamberte.com>
2024-08-26 04:26:36 -0700

    adding verfical blip clock, start logic

--- vBlip.js
+++ vBlip.js
@@ -1,1 +1,14 @@
-console.log("vBlipn is Online");
+console.log('vBlipn is Online');
+
+const blipDecimalHour10BinaryHour8 = document.getElementById('blipD10BH8');
+
+function updateVerticalBlipClock() {
+	const now = new Date();
+	const decimalHour10 = Math.floor(now.getHours() / 10);
+	const decimalHour1 = now.getHours() % 10;
+
+	console.log(decimalHour10);
+	console.log(decimalHour1);
+}
+
+updateVerticalBlipClock();
