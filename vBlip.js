08b8291c7bb284f35c63fe0c3001de159d9ba9b9
Travis Lamberte <travis@lamberte.com>
2024-08-27 03:24:15 -0700

    add blip status

--- vBlip.js
+++ vBlip.js
@@ -19,6 +19,36 @@ function updateVerticalBlipClock() {
 	const binarySec10 = decimalSec10.toString(2).padStart(3, '0');
 	const binarySec1 = decimalSec1.toString(2).padStart(4, '0');
 
+	// Name format is Unit Hr/Min/Sec followed by base 10 followed by base 2 example Hr10_4
+	const Hr10_4 = binaryHr10.charAt(2);
+	const Hr10_2 = binaryHr10.charAt(1);
+	const Hr10_1 = binaryHr10.charAt(0);
+	const Hr1_8 = binaryHr1.charAt(3);
+	const Hr1_4 = binaryHr1.charAt(2);
+	const Hr1_2 = binaryHr1.charAt(1);
+	const Hr1_1 = binaryHr1.charAt(0);
+	const Min10_4 = binaryMin10.charAt(2);
+	const Min10_2 = binaryMin10.charAt(1);
+	const Min10_1 = binaryMin10.charAt(0);
+	const Min1_8 = binaryMin1.charAt(3);
+	const Min1_4 = binaryMin1.charAt(2);
+	const Min1_2 = binaryMin1.charAt(1);
+	const Min1_1 = binaryMin1.charAt(0);
+	const Sec10_4 = binarySec10.charAt(2);
+	const Sec10_2 = binarySec10.charAt(1);
+	const Sec10_1 = binarySec10.charAt(0);
+	const Sec1_8 = binarySec1.charAt(3);
+	const Sec1_4 = binarySec1.charAt(2);
+	const Sec1_2 = binarySec1.charAt(1);
+	const Sec1_1 = binarySec1.charAt(0);
+
+	vBlipHr10_4.className =
+		Hr10_4 == '1' ? 'blipperOn blips' : 'blipperOff blips';
+	vBlipHr10_2.className =
+		Hr10_2 == '1' ? 'blipperOn blips' : 'blipperOff blips';
+	vBlipHr10_1.className =
+		Hr10_1 == '1' ? 'blipperOn blips' : 'blipperOff blips';
+
 	const readout = `${binaryHr10} ${binaryHr1} : ${binaryMin10} ${binaryMin1} : ${binarySec10} ${binarySec1}`;
 
 	displayVBlip.innerText = readout;
