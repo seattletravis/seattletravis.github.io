eb78c673a34993336a5415106000c7693af5e7ef
Travis Lamberte <travis@lamberte.com>
2024-08-30 04:53:43 -0700

    installed NPM into project

---
+++ vBlipperClock.js
@@ -0,0 +1,9 @@
+// import * as clockLibrary from 'binary-clock-core';
+const clock = require('binary-clock-core');
+
+vBlipperClock = () => {
+	const now = new Date();
+	console.log(now);
+};
+
+vBlipperClock();
