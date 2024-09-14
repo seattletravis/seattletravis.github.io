6d6bb8885671597480ae772ade614932d6b589ca
Travis Lamberte <travis@lamberte.com>
2024-09-14 06:27:31 -0700

    add logic to pull first and second places off seconds hand of time to convert each hand to binary

--- BinaryModule.js
+++ BinaryModule.js
@@ -1,32 +1,38 @@
 export function binaryClockData() {
-  console.log("Here's your object with all the stuff you need in it!!!");
+	console.log("Here's your object with all the stuff you need in it!!!");
 }
 
 export class testClass {
-  constructor(nowNow) {
-    let secondsInBinary = nowNow.getSeconds().toString(2).padStart(6, "0");
-    let minutesInBinary = nowNow.getMinutes().toString(2).padStart(6, "0");
-    let hoursInBinary = nowNow.getHours().toString(2).padStart(6, "0");
-    this.secondsInBinary = secondsInBinary;
-    this.sec11_32 = secondsInBinary.charAt(0) == "1" ? true : false;
-    this.sec11_16 = secondsInBinary.charAt(1) == "1" ? true : false;
-    this.sec11_08 = secondsInBinary.charAt(2) == "1" ? true : false;
-    this.sec11_04 = secondsInBinary.charAt(3) == "1" ? true : false;
-    this.sec11_02 = secondsInBinary.charAt(4) == "1" ? true : false;
-    this.sec11_01 = secondsInBinary.charAt(5) == "1" ? true : false;
-    this.minutesInBinary = minutesInBinary;
-    this.min11_32 = minutesInBinary.charAt(0) == "1" ? true : false;
-    this.min11_16 = minutesInBinary.charAt(1) == "1" ? true : false;
-    this.min11_08 = minutesInBinary.charAt(2) == "1" ? true : false;
-    this.min11_04 = minutesInBinary.charAt(3) == "1" ? true : false;
-    this.min11_02 = minutesInBinary.charAt(4) == "1" ? true : false;
-    this.min11_01 = minutesInBinary.charAt(5) == "1" ? true : false;
-    this.hoursInBinary = hoursInBinary;
-    this.hr11_32 = hoursInBinary.charAt(0) == "1" ? true : false;
-    this.hr11_16 = hoursInBinary.charAt(1) == "1" ? true : false;
-    this.hr11_08 = hoursInBinary.charAt(2) == "1" ? true : false;
-    this.hr11_04 = hoursInBinary.charAt(3) == "1" ? true : false;
-    this.hr11_02 = hoursInBinary.charAt(4) == "1" ? true : false;
-    this.hr11_01 = hoursInBinary.charAt(5) == "1" ? true : false;
-  }
+	constructor(nowNow) {
+		let seconds11InBinary = nowNow.getSeconds().toString(2).padStart(6, '0');
+		let minutes11InBinary = nowNow.getMinutes().toString(2).padStart(6, '0');
+		let hours11InBinary = nowNow.getHours().toString(2).padStart(6, '0');
+		let seconds01InBinary = nowNow.getSeconds() % 10;
+		let seconds10InBinary = Math.floor(nowNow.getSeconds() / 10);
+
+		this.sec10 = seconds10InBinary;
+		this.sec01 = seconds01InBinary;
+
+		this.sec11 = seconds11InBinary;
+		this.sec11_32 = seconds11InBinary.charAt(0) == '1' ? true : false;
+		this.sec11_16 = seconds11InBinary.charAt(1) == '1' ? true : false;
+		this.sec11_08 = seconds11InBinary.charAt(2) == '1' ? true : false;
+		this.sec11_04 = seconds11InBinary.charAt(3) == '1' ? true : false;
+		this.sec11_02 = seconds11InBinary.charAt(4) == '1' ? true : false;
+		this.sec11_01 = seconds11InBinary.charAt(5) == '1' ? true : false;
+		this.min11 = minutes11InBinary;
+		this.min11_32 = minutes11InBinary.charAt(0) == '1' ? true : false;
+		this.min11_16 = minutes11InBinary.charAt(1) == '1' ? true : false;
+		this.min11_08 = minutes11InBinary.charAt(2) == '1' ? true : false;
+		this.min11_04 = minutes11InBinary.charAt(3) == '1' ? true : false;
+		this.min11_02 = minutes11InBinary.charAt(4) == '1' ? true : false;
+		this.min11_01 = minutes11InBinary.charAt(5) == '1' ? true : false;
+		this.hr11 = hours11InBinary;
+		this.hr11_32 = hours11InBinary.charAt(0) == '1' ? true : false;
+		this.hr11_16 = hours11InBinary.charAt(1) == '1' ? true : false;
+		this.hr11_08 = hours11InBinary.charAt(2) == '1' ? true : false;
+		this.hr11_04 = hours11InBinary.charAt(3) == '1' ? true : false;
+		this.hr11_02 = hours11InBinary.charAt(4) == '1' ? true : false;
+		this.hr11_01 = hours11InBinary.charAt(5) == '1' ? true : false;
+	}
 }
