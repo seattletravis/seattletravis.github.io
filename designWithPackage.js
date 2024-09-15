2efd2e688e9ecb4607abd7946c94d2851a4f7812
Travis Lamberte <travis@lamberte.com>
2024-09-15 08:25:00 -0700

    writing more readable code

--- designWithPackage.js
+++ designWithPackage.js
@@ -1,9 +1,10 @@
 import { timeInBinary } from './TimeInBinary.js';
+const baseTenClock = document.getElementById('tib1TimeInBase10');
 
 function updateTib1() {
 	const tib1 = new timeInBinary();
-	console.log(tib1.hrsInDec + ' : ' + tib1.minInDec + ' : ' + tib1.secInDec);
-
+	baseTenClock.innerHTML =
+		tib1.hrsInDec + ' : ' + tib1.minInDec + ' : ' + tib1.secInDec;
 	setTimeout(updateTib1, 1000);
 }
 
