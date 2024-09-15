cd39ee93f3fa02b944448af139f2de97499293a4
Travis Lamberte <travis@lamberte.com>
2024-09-15 08:04:53 -0700

    set up new project to build out new design

---
+++ designWithPackage.js
@@ -0,0 +1,7 @@
+import { timeInBinary } from './TimeInBinary.js';
+
+const tib1 = new timeInBinary();
+
+console.log(tib1.hrs11);
+
+console.log(tib1.hrsInDec + ' : ' + tib1.minInDec + ' : ' + tib1.secInDec);
