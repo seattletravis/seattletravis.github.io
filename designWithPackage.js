f7d01ea044cd8e3853e5a768d0447e16ceb3e1de
Travis Lamberte <travis@lamberte.com>
2024-09-15 09:19:48 -0700

    listed the id's in the html file

--- designWithPackage.js
+++ designWithPackage.js
@@ -1,9 +1,38 @@
 import { timeInBinary } from './TimeInBinary.js';
-const baseTenClock = document.getElementById('tib1TimeInBase10');
+const tib1TimeInBase10 = document.getElementById('tib1TimeInBase10');
+
+const elements = [
+	'tib1TimeInBase10',
+	'tib1',
+	'tib1Hr10_01',
+	'tib1Hr10_02',
+	'tib1Hr10_04',
+	'tib1Hr10_08',
+	'tib1Hr01_01',
+	'tib1Hr01_02',
+	'tib1Hr01_04',
+	'tib1Hr01_08',
+	'tib1Min10_01',
+	'tib1Min10_02',
+	'tib1Min10_04',
+	'tib1Min10_08',
+	'tib1Min01_01',
+	'tib1Min01_02',
+	'tib1Min01_04',
+	'tib1Min01_08',
+	'tib1Sec10_01',
+	'tib1Sec10_02',
+	'tib1Sec10_04',
+	'tib1Sec10_08',
+	'tib1Sec01_01',
+	'tib1Sec01_02',
+	'tib1Sec01_04',
+	'tib1Sec01_08',
+];
 
 function updateTib1() {
 	const tib1 = new timeInBinary();
-	baseTenClock.innerHTML =
+	tib1TimeInBase10.innerHTML =
 		tib1.hrsInDec + ' : ' + tib1.minInDec + ' : ' + tib1.secInDec;
 	setTimeout(updateTib1, 1000);
 }
