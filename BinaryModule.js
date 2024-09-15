4ad30f59c41d3509f6da6cd7b1862f717c01fbd3
Travis Lamberte <travis@lamberte.com>
2024-09-14 17:44:02 -0700

    refactor names of variables

--- BinaryModule.js
+++ BinaryModule.js
@@ -3,92 +3,88 @@ export class testClass {
 		const nowNow = new Date();
 		if ((hourMode = '12')) {
 		}
-		let seconds11InBinary = nowNow.getSeconds().toString(2).padStart(6, '0');
-		let minutes11InBinary = nowNow.getMinutes().toString(2).padStart(6, '0');
-		let hours11InBinary = nowNow.getHours().toString(2).padStart(6, '0');
-		let seconds01InBinary = (nowNow.getSeconds() % 10)
-			.toString(2)
-			.padStart(4, '0');
-		let seconds10InBinary = Math.floor(nowNow.getSeconds() / 10)
+		let sec11InBin = nowNow.getSeconds().toString(2).padStart(6, '0');
+		let min11InBin = nowNow.getMinutes().toString(2).padStart(6, '0');
+		let hrs11InBin = nowNow.getHours().toString(2).padStart(6, '0');
+		let sec01InBin = (nowNow.getSeconds() % 10).toString(2).padStart(4, '0');
+		let sec10InBin = Math.floor(nowNow.getSeconds() / 10)
 			.toString(2)
 			.padStart(4, '0');
 
-		let minutes01InBinary = (nowNow.getMinutes() % 10)
+		let min01InBin = (nowNow.getMinutes() % 10).toString(2).padStart(4, '0');
+		let min10InBin = Math.floor(nowNow.getMinutes() / 10)
 			.toString(2)
 			.padStart(4, '0');
-		let minutes10InBinary = Math.floor(nowNow.getMinutes() / 10)
-			.toString(2)
-			.padStart(4, '0');
-		let hours01InBinary = (nowNow.getHours() % 10).toString(2).padStart(4, '0');
-		let hours10InBinary = Math.floor(nowNow.getHours() / 10)
+		let hrs01InBin = (nowNow.getHours() % 10).toString(2).padStart(4, '0');
+		let hrs10InBin = Math.floor(nowNow.getHours() / 10)
 			.toString(2)
 			.padStart(4, '0');
 
 		// Split Hand to Binary - Hours, '10' - 1st Hand | '01' - 2nd Hand
-		this.hr10 = hours10InBinary;
+		this.hrs10 = hrs10InBin;
 		// hours 1st hand
-		this.hr10_08 = false;
-		this.hr10_04 = hours10InBinary.charAt(1) == '1' ? true : false;
-		this.hr10_02 = hours10InBinary.charAt(2) == '1' ? true : false;
-		this.hr10_01 = hours10InBinary.charAt(3) == '1' ? true : false;
+		this.hrs10_08 = false;
+		this.hrs10_04 = hrs10InBin.charAt(1) == '1' ? true : false;
+		this.hrs10_02 = hrs10InBin.charAt(2) == '1' ? true : false;
+		this.hrs10_01 = hrs10InBin.charAt(3) == '1' ? true : false;
 		// hours 2nd hand
-		this.hr01 = hours01InBinary;
-		this.hr01_08 = hours01InBinary.charAt(0) == '1' ? true : false;
-		this.hr01_04 = hours01InBinary.charAt(1) == '1' ? true : false;
-		this.hr01_02 = hours01InBinary.charAt(2) == '1' ? true : false;
-		this.hr01_01 = hours01InBinary.charAt(3) == '1' ? true : false;
+		this.hrs01 = hrs01InBin;
+		this.hrs01_08 = hrs01InBin.charAt(0) == '1' ? true : false;
+		this.hrs01_04 = hrs01InBin.charAt(1) == '1' ? true : false;
+		this.hrs01_02 = hrs01InBin.charAt(2) == '1' ? true : false;
+		this.hrs01_01 = hrs01InBin.charAt(3) == '1' ? true : false;
 
 		// Split Hand to Binary - Minutes, '10' - 1st Hand | '01' - 2nd Hand
-		this.min10 = minutes10InBinary;
+		this.min10 = min10InBin;
 		// minutes 1st hand
 		this.min10_08 = false;
-		this.min10_04 = minutes10InBinary.charAt(1) == '1' ? true : false;
-		this.min10_02 = minutes10InBinary.charAt(2) == '1' ? true : false;
-		this.min10_01 = minutes10InBinary.charAt(3) == '1' ? true : false;
+		this.min10_04 = min10InBin.charAt(1) == '1' ? true : false;
+		this.min10_02 = min10InBin.charAt(2) == '1' ? true : false;
+		this.min10_01 = min10InBin.charAt(3) == '1' ? true : false;
 		// minutes 2nd hand
-		this.min01 = minutes01InBinary;
-		this.min01_08 = minutes01InBinary.charAt(0) == '1' ? true : false;
-		this.min01_04 = minutes01InBinary.charAt(1) == '1' ? true : false;
-		this.min01_02 = minutes01InBinary.charAt(2) == '1' ? true : false;
-		this.min01_01 = minutes01InBinary.charAt(3) == '1' ? true : false;
+		this.min01 = min01InBin;
+		this.min01_08 = min01InBin.charAt(0) == '1' ? true : false;
+		this.min01_04 = min01InBin.charAt(1) == '1' ? true : false;
+		this.min01_02 = min01InBin.charAt(2) == '1' ? true : false;
+		this.min01_01 = min01InBin.charAt(3) == '1' ? true : false;
 
 		// Split Hand to Binary - Seconds, '10' - 1st Hand | '01' - 2nd Hand
-		this.sec10 = seconds10InBinary;
+		this.sec10 = sec10InBin;
 		// seconds 1st hand
 		this.sec10_08 = false;
-		this.sec10_04 = seconds10InBinary.charAt(1) == '1' ? true : false;
-		this.sec10_02 = seconds10InBinary.charAt(2) == '1' ? true : false;
-		this.sec10_01 = seconds10InBinary.charAt(3) == '1' ? true : false;
+		this.sec10_04 = sec10InBin.charAt(1) == '1' ? true : false;
+		this.sec10_02 = sec10InBin.charAt(2) == '1' ? true : false;
+		this.sec10_01 = sec10InBin.charAt(3) == '1' ? true : false;
 		// seconds 2nd hand
-		this.sec01 = seconds01InBinary;
-		this.sec01_08 = seconds01InBinary.charAt(0) == '1' ? true : false;
-		this.sec01_04 = seconds01InBinary.charAt(1) == '1' ? true : false;
-		this.sec01_02 = seconds01InBinary.charAt(2) == '1' ? true : false;
-		this.sec01_01 = seconds01InBinary.charAt(3) == '1' ? true : false;
+		this.sec01 = sec01InBin;
+		this.sec01_08 = sec01InBin.charAt(0) == '1' ? true : false;
+		this.sec01_04 = sec01InBin.charAt(1) == '1' ? true : false;
+		this.sec01_02 = sec01InBin.charAt(2) == '1' ? true : false;
+		this.sec01_01 = sec01InBin.charAt(3) == '1' ? true : false;
 
 		// Full Hand to Binary - Seconds
-		this.sec11 = seconds11InBinary;
-		this.sec11_32 = seconds11InBinary.charAt(0) == '1' ? true : false;
-		this.sec11_16 = seconds11InBinary.charAt(1) == '1' ? true : false;
-		this.sec11_08 = seconds11InBinary.charAt(2) == '1' ? true : false;
-		this.sec11_04 = seconds11InBinary.charAt(3) == '1' ? true : false;
-		this.sec11_02 = seconds11InBinary.charAt(4) == '1' ? true : false;
-		this.sec11_01 = seconds11InBinary.charAt(5) == '1' ? true : false;
+		this.sec11 = sec11InBin;
+		this.sec11_32 = sec11InBin.charAt(0) == '1' ? true : false;
+		this.sec11_16 = sec11InBin.charAt(1) == '1' ? true : false;
+		this.sec11_08 = sec11InBin.charAt(2) == '1' ? true : false;
+		this.sec11_04 = sec11InBin.charAt(3) == '1' ? true : false;
+		this.sec11_02 = sec11InBin.charAt(4) == '1' ? true : false;
+		this.sec11_01 = sec11InBin.charAt(5) == '1' ? true : false;
 		// Full Hand to Binary - Minutes
-		this.min11 = minutes11InBinary;
-		this.min11_32 = minutes11InBinary.charAt(0) == '1' ? true : false;
-		this.min11_16 = minutes11InBinary.charAt(1) == '1' ? true : false;
-		this.min11_08 = minutes11InBinary.charAt(2) == '1' ? true : false;
-		this.min11_04 = minutes11InBinary.charAt(3) == '1' ? true : false;
-		this.min11_02 = minutes11InBinary.charAt(4) == '1' ? true : false;
-		this.min11_01 = minutes11InBinary.charAt(5) == '1' ? true : false;
+		this.min11 = min11InBin;
+		this.min11_32 = min11InBin.charAt(0) == '1' ? true : false;
+		this.min11_16 = min11InBin.charAt(1) == '1' ? true : false;
+		this.min11_08 = min11InBin.charAt(2) == '1' ? true : false;
+		this.min11_04 = min11InBin.charAt(3) == '1' ? true : false;
+		this.min11_02 = min11InBin.charAt(4) == '1' ? true : false;
+		this.min11_01 = min11InBin.charAt(5) == '1' ? true : false;
 		// Full Hand to Binany - Hours
-		this.hr11 = hours11InBinary;
-		this.hr11_32 = false;
-		this.hr11_16 = false;
-		this.hr11_08 = hours11InBinary.charAt(2) == '1' ? true : false;
-		this.hr11_04 = hours11InBinary.charAt(3) == '1' ? true : false;
-		this.hr11_02 = hours11InBinary.charAt(4) == '1' ? true : false;
-		this.hr11_01 = hours11InBinary.charAt(5) == '1' ? true : false;
+		this.hrs11 = hrs11InBin;
+		this.hrs11_32 = false;
+		this.hrs11_16 = false;
+		this.hrs11_08 = hrs11InBin.charAt(2) == '1' ? true : false;
+		this.hrs11_04 = hrs11InBin.charAt(3) == '1' ? true : false;
+		this.hrs11_02 = hrs11InBin.charAt(4) == '1' ? true : false;
+		this.hrs11_01 = hrs11InBin.charAt(5) == '1' ? true : false;
 	}
 }
--- app.js
+++ app.js
@@ -3,28 +3,64 @@ import { testClass } from './BinaryModule.js';
 // test import of class from BinaryModule.js
 // const nowNow = new Date();
 const now = new testClass();
-console.log(now.hr11);
-console.log('Split Hour Hands ' + now.hr10 + ' ' + now.hr01);
+console.log(now.hrs11);
+console.log('Split Hour Hands: ' + now.hrs10 + ' | ' + now.hrs01);
 console.log(
-	now.hr10_08 + ' ' + now.hr10_04 + ' ' + now.hr10_02 + ' ' + now.hr10_01
+	now.hrs10_08 +
+		' | ' +
+		now.hrs10_04 +
+		' | ' +
+		now.hrs10_02 +
+		' | ' +
+		now.hrs10_01
 );
 console.log(
-	now.hr01_08 + ' ' + now.hr01_04 + ' ' + now.hr01_02 + ' ' + now.hr01_01
+	now.hrs01_08 +
+		' | ' +
+		now.hrs01_04 +
+		' | ' +
+		now.hrs01_02 +
+		' | ' +
+		now.hrs01_01
 );
-console.log('Split Minute Hands ' + now.min10 + ' ' + now.min01);
+console.log('Split Minute Hands: ' + now.min10 + ' | ' + now.min01);
 console.log(
-	now.min10_08 + ' ' + now.min10_04 + ' ' + now.min10_02 + ' ' + now.min10_01
+	now.min10_08 +
+		' | ' +
+		now.min10_04 +
+		' | ' +
+		now.min10_02 +
+		' | ' +
+		now.min10_01
 );
 console.log(
-	now.min01_08 + ' ' + now.min01_04 + ' ' + now.min01_02 + ' ' + now.min01_01
+	now.min01_08 +
+		' | ' +
+		now.min01_04 +
+		' | ' +
+		now.min01_02 +
+		' | ' +
+		now.min01_01
 );
 
-console.log('Split Second Hands ' + now.sec10 + ' ' + now.sec01);
+console.log('Split Second Hands: ' + now.sec10 + ' | ' + now.sec01);
 console.log(
-	now.sec10_08 + ' ' + now.sec10_04 + ' ' + now.sec10_02 + ' ' + now.sec10_01
+	now.sec10_08 +
+		' | ' +
+		now.sec10_04 +
+		' | ' +
+		now.sec10_02 +
+		' | ' +
+		now.sec10_01
 );
 console.log(
-	now.sec01_08 + ' ' + now.sec01_04 + ' ' + now.sec01_02 + ' ' + now.sec01_01
+	now.sec01_08 +
+		' | ' +
+		now.sec01_04 +
+		' | ' +
+		now.sec01_02 +
+		' | ' +
+		now.sec01_01
 );
 
 console.log("It's telling time, it's really really telling time!!!");
